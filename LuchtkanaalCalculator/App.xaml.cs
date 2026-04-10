4ea8532f18267ff4d2a344bcf924b3b2c510eef1
Antoine van den Hurk <antoinevandenhurk@hotmail.com>
2026-04-10 18:39:39 +0200

    Initial commit: Air duct calculator with logo icon and Dutch UI

---
+++ LuchtkanaalCalculator/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace LuchtkanaalCalculator;
+
+/// <summary>
+/// Interactielogica voor App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
